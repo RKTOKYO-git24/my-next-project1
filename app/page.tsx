270ed571ae17d933e0595805bf886070f7050dd8
RK TOKYO <kryo71980@gmail.com>
2025-01-02 02:40:20 +0000

    css next/image

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+import styles from "./page.module.css";
+import Image from "next/image"; // Importing the Image component
+
 export default function Home() {
-  //JavaScriptの定数や式などを書くことができる
-  const name = "World";
-
-  //htmlのようなマークアップを書くことができる
-  // javascriptを書いたり、参照する際には{}で囲む
-
   return (
-    <div>
-      <h1>テクノロジーの力で{name}を変える</h1>
-      <p>私たちは市場をリードしているグローバルテックカンパニーです。</p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力でを変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt="Background image"
+        layout="fill" /* Makes the image cover the container */
+        objectFit="cover" /* Ensures the image covers the section properly */
+        priority /* Optional: loads the image earlier for better LCP */
+      />
+    </section>
   );
 }
