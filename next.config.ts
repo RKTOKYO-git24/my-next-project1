3b71ed082e8846f51ac04c6a1d349b654111a612
RK TOKYO <kryo71980@gmail.com>
2025-01-21 16:10:22 +0000

    headless

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+/** .mjs codes described in github have been changed for .ts here */
