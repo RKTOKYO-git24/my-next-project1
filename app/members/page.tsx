738a6d70ae8731977bba5873c9183ebb739f36be
RK TOKYO <kryo71980@gmail.com>
2025-01-30 02:10:37 +0000

    API3-chapter5

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { getMemberList } from "../_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
+import styles from "./page.module.css";
 
 export default async function Page() {
-  const data = await getMemberList();
+  const data = await getMemberList({ limit: MEMBERS_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
