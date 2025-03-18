63d92cfc3c3d62637b992342b0fe061d3a51f480
RK TOKYO <kryo71980@gmail.com>
2025-03-18 01:11:39 +0000

    VercelError1-6

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,36 +4,14 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-// The params are automatically passed in the component by Next.js for dynamic routes
 type Props = {
   params: {
-    slug: string; // `slug` will be passed dynamically based on your URL structure
+    slug: string;
   };
 };
 
-// Generate Metadata (Optional, for SEO/meta tags)
-export async function generateMetadata({ params }: Props) {
-  const data = await getNewsDetail(params.slug).catch(() => null);
-
-  if (!data) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    title: data.title,
-    description: data.description,
-  };
-}
-
-// Page Component where dynamic params (slug) are passed automatically
 export default async function Page({ params }: Props) {
-  const data = await getNewsDetail(params.slug).catch(notFound); // Fetch data based on the slug
-
-  if (!data) {
-    return null; // Show fallback or error state
-  }
+  const data = await getNewsDetail(params.slug).catch(notFound);
 
   return (
     <>
