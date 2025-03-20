659a202d90b4f4e31cb3c3cc315936a15e3fec51
RK TOKYO <kryo71980@gmail.com>
2025-03-20 11:42:21 +0000

    after deploy3-2

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -6,14 +6,13 @@ import styles from "./page.module.css";
 
 // Correctly type the params to use Promise
 type Props = {
-  params: Promise<{
+  params: {
     slug: string;
-  }>;
+  };
   searchParams: {
     dk?: string;
   };
 };
-
 export default async function Page({ params, searchParams }: Props) {
   // Ensure params is resolved properly as a Promise
   const data = await getNewsDetail((await params).slug, {
