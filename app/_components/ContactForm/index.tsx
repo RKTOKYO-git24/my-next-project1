63692e7855cde83bdbf8e58959e753b0bb9a2622
RK TOKYO <kryo71980@gmail.com>
2025-03-25 02:32:19 +0000

    after deploy3-4

--- app/_components/ContactForm/index.tsx
+++ app/_components/ContactForm/index.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import { createContactData } from "@/app/_actions/contact";
+import { useActionState } from "react";
 import styles from "./index.module.css";
 
+const initialState = {
+  status: "",
+  message: "",
+};
+
 export default function ContactForm() {
+  const [state, formAction] = useActionState(createContactData, initialState);
+  console.log(state);
+  if (state?.status === "success") {
+    return (
+      <p className={styles.success}>
+        お問い合わせいただき、ありがとうございます。
+        <br />
+        お返事まで今しばらくお待ちください。
+      </p>
+    );
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} action={formAction}>
       <div className={styles.horizontal}>
         <div className={styles.item}>
           <label className={styles.label} htmlFor="lastname">
@@ -22,7 +43,7 @@ export default function ContactForm() {
           </label>
           <input
             className={styles.textfield}
-            type="texst"
+            type="text"
             id="firstname"
             name="firstname"
           />
@@ -58,6 +79,9 @@ export default function ContactForm() {
         <textarea className={styles.textarea} id="message" name="message" />
       </div>
       <div className={styles.actions}>
+        {state?.status === "error" && (
+          <p className={styles.error}>{state.message}</p>
+        )}
         <input type="submit" value="送信する" className={styles.button} />
       </div>
     </form>
