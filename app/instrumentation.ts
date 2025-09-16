8af32ac45db229de0ce15292687044d6230a15b8
RKTOKYO <kryo71980@gmail.com>
2025-09-17 01:21:58 +0900

    sentry3

--- app/instrumentation.ts
+++ app/instrumentation.ts
@@ -6,7 +6,7 @@ console.log("🚀 instrumentation.ts loaded");
 
 export function register() {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN, // 👈 修正！
     tracesSampleRate: 1.0,
     replaysSessionSampleRate: 0.1,
     replaysOnErrorSampleRate: 1.0,
